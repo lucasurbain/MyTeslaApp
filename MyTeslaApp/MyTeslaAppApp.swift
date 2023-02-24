12269551bef503ab43224b068666895df36c3e67
lucasurbain <lucasurbain68@protonmail.ch>
2023-02-24 10:49:21 +0100

    Initial Commit

---
+++ MyTeslaApp/MyTeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTeslaAppApp.swift
+//  MyTeslaApp
+//
+//  Created by lucas urbain on 24.02.23.
+//
+
+import SwiftUI
+
+@main
+struct MyTeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
